9960b76600fbb18764b7265147ed6953799ec4a7
root <root@Python>
2025-06-16 17:45:38 -0300

    Checkpoint: Tela inicial funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'pages/medications_page.dart';
-import 'medications_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +7,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
