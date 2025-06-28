b3a253aa4ffa23de361751582083c5995b7df08e
RGarcez <rgs.dba7@gmail.com>
2025-06-27 22:25:13 -0300

    Add empty pages for Exams & Health Data

---
+++ lib/pages/exams_consultations_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ExamsConsultationsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Exames e Consultas - Em desenvolvimento',
+        style: TextStyle(fontSize: 18, color: Colors.grey),
+      ),
+    );
+  }
+}
---
+++ lib/pages/health_data_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HealthDataPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Dados de Saúde - Em desenvolvimento',
+        style: TextStyle(fontSize: 18, color: Colors.grey),
+      ),
+    );
+  }
+}
